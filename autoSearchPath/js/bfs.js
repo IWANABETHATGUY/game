b34c302044ff2d41c1d144c174cacadda6242056
IWANABETHATGUY <974153916@qq.com>
2020-01-05 23:18:14 +0800

    feat(bfs): bfs

--- autoSearchPath/js/bfs.js
+++ autoSearchPath/js/bfs.js
@@ -1,149 +1,76 @@
-class Node {
-    constructor(value, priority, parent = null) {
-        this.value = value;
-        this.priority = priority;
-        this.parent = parent;
-    }
-}
-class PriorityQueue {
-    constructor() {
-        this.items = [];
-    }
-    insert(value, priority, parent) {
-        let queObj = new Node(value, priority, parent); //创建队列元素对象
-        if (this.isEmpty()) { //如果队列是空的，直接插入
-            this.items.push(queObj);
-        } else {
-            let bAdded = false;
-            for (let i = 0, len = this.items.length; i < len; i++) { 
-                if (priority < this.items[i].priority) {
-                    this.items.splice(i, 0, queObj); // 循环队列，如果优先级小于这个位置元素的优先级，插入
-                    bAdded = true;
-                    break;
-                }
-            }
-            if (!bAdded) {
-                this.items.push(queObj); // 如果循环一圈都没有找到能插队的位置，直接插入队列尾部
-            }
-        }
-    }
-    get() {
-        return this.items.shift();
-    }
-    isEmpty() {
-        return this.items.length === 0;
-    }
-}
+class BFS {
+  constructor(grid, start, end, R, C) {
+    this.grid = grid;
+    this.start = start;
+    this.end = end;
+    this.Row = R;
+    this.Col = C;
+    this.pre = Array.from({ length: 10 }).map(arr =>
+      Array.from({ length: 10 }).fill(-1)
+    );
+    this.marked = Array.from({ length: 10 }).map(arr =>
+      Array.from({ length: 10 }).fill(0)
+    );
+  }
 
-class AFind {
-    constructor(matrix, startR, startC, endR, endC, row, col) {
-        this.matrix = matrix;
-        this.startR = startR;
-        this.startC = startC;
-        this.endR = endR;
-        this.endC = endC;
-        this.row = row;
-        this.col = col;
-        this.Hlist = this.getHlist();
-        this.Glist = this.getGlist();
-        if (!this.arr || !this.row, !this.col) {
-            throw Error('arguments is not legal need 3');
+  findPath() {
+    let start = this.start[0] * this.Col + this.start[1];
+    let dirs = [
+      [-1, 0],
+      [0, 1],
+      [1, 0],
+      [0, -1],
+    ];
+    let q = [start];
+    let r = Math.floor(start / this.Col);
+    let c = start % this.Col;
+    this.marked[r][c] = 1;
+    let flag;
+    while (q.length) {
+      flag = false;
+      let cur = q.shift();
+      let r = Math.floor(cur / this.Col);
+      let c = cur % this.Col;
+      
+      for (let i = 0; i < dirs.length; i++) {
+        let nr = r + dirs[i][0];
+        let nc = c + dirs[i][1];
+        if (
+          this.inArea(nr, nc) &&
+          this.grid[nr][nc] !== 1 &&
+          !this.marked[nr][nc]
+        ) {
+          this.marked[nr][nc] = 1;
+          let pos = nr * this.Col + nc;
+          q.push(pos);
+          this.pre[nr][nc] = cur;
         }
-        this.direction = [
-            [-1, 0],
-            [1, 0],
-            [0, -1],
-            [0, 1]
-        ];
-        this.marked = this.getMatrix();
-    }
-    //检测当前位置是否合法
-    isLegal(row, col, matrix, marked) {
-        return row >= 0 && row < 10 && col >= 0 && col < 10 && !marked[row][col] && matrix[row][col] !== 1;
-    }
-    //得到一个新的矩阵
-    getMatrix() {
-        let arr = [];
-        for (let i = 0; i < this.row; i++) {
-            arr[i] = [];
-            for (let j = 0; j < this.col; j++) {
-                arr[i][j] = 0;
-            }
+        if (nr === this.end[0] && nc === this.end[1]) {
+          flag = true
+          break;
         }
-        return arr;
+      }
+      if (flag) {
+        break;
+      }
     }
-    find(path) {
-        let List = new PriorityQueue(),
-            cur = null,
-            result = null;
-        List.insert([this.startR, this.startC], this.Hlist[this.startR][this.startC]);
-        while (!List.isEmpty()) {
-            cur = List.get();
-            let r = cur.value[0],
-                c = cur.value[1];
-            this.marked[r][c] = 1;
-            if (r === this.endR && c === this.endC) {
-                result = cur;
-                break;
-            }
-            for (let i = 0, len = this.direction.length; i < len; i++) {
-                let newR = r + this.direction[i][0],
-                    newC = c + this.direction[i][1];
-                if (this.isLegal(newR, newC, this.matrix, this.marked)) {
-                    this.Glist[newR][newC] = this.Glist[r][c] + 1;
-                    List.insert([newR, newC], this.Hlist[newR][newC] + this.Glist[newR][newC], cur);
-                }
-            }
-        }
-        if (result) {
-            while (result.parent) {
-                path.push(result.value);
-                result = result.parent;
-            }
-            path.push([this.startR, this.startC]);
-            return path.reverse();
-        }
-        return path;
+    if (flag) {
+      let resPath = [];
+      let cur = this.end[0] * this.Col + this.end[1];
+      while (cur !== start) {
+        let r = Math.floor(cur / this.Col);
+        let c = cur % this.Col;
+        resPath.push([r, c]);
+        cur = this.pre[r][c]
+      }
+      resPath.push([r, c]);
+      resPath.reverse()
+      console.log(resPath)
+      return resPath;
     }
-    getPath() {
-        let path = [];
-        this.find(path);
-        return path;
-    }
-    getDistace(a, b) {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-    }
-    getHlist() {
-        let cur = [];
-        copy(cur, this.matrix);
-        let l = cur[0].length,
-            endPosition = [this.endR, this.endC];
-        for (let i = 0, len = cur.length; i < len; i++) {
-            for (let j = 0; j < l; j++) {
-                cur[i][j] = this.getDistace([i, j], endPosition);
-            }
-        }
-        return cur;
-    }
-    getGlist() {
-        let cur = [];
-        copy(cur, this.matrix);
-        let l = cur[0].length,
-            endPosition = [this.startR, this.startC];
-        for (let i = 0, len = cur.length; i < len; i++) {
-            for (let j = 0; j < l; j++) {
-                cur[i][j] = this.getDistace([i, j], endPosition);
-            }
-        }
-        return cur;
-    }
-}
-
-function copy (a, b) {
-    for (let i = 0, len = b.length; i < len; i++) {
-        a[i] = [];
-        for (let j = 0, l = b[0].length; j < l; j++) {
-            a[i][j] = b[i][j];
-        }
-    }
+    return []
+  }
+  inArea(r, c) {
+    return r >= 0 && r < this.Row && c >= 0 && c < this.Col;
+  }
 }
--- autoSearchPath/js/main.js
+++ autoSearchPath/js/main.js
@@ -1,159 +1,181 @@
 let controler = document.getElementsByClassName('controler')[0],
-    container = document.getElementsByClassName('container')[0],
-    button = document.getElementById('button'),
-    aFind = document.getElementById('aFind'),
-    startElement = null,
-    endElement = null,
-    activeHelper = null,
-    posArray = [],
-    quickBuild = false;
+  container = document.getElementsByClassName('container')[0],
+  dfs = document.getElementById('dfs'),
+  aFind = document.getElementById('aFind'),
+  startElement = null,
+  endElement = null,
+  activeHelper = null,
+  posArray = [],
+  quickBuild = false;
 init();
 
 //
 aFind.addEventListener('click', () => {
-    let result = getMatrix(posArray),
-        A = new AFind(result.arr, result.start[0], result.start[1], result.end[0], result.end[1], 10, 10),
-        path = A.getPath();
-    for (let i = 0, len = path.length; i < len; i++) {
-        setTimeout(() => {
-            posArray[path[i][0]][path[i][1]].classList.add('active');
-        }, i * 200);
-        setTimeout(() => {
-            posArray[path[i][0]][path[i][1]].classList.remove('active');
-        }, i * 200 + 200);
-    }
+  let result = getMatrix(posArray),
+    A = new AFind(
+      result.arr,
+      result.start[0],
+      result.start[1],
+      result.end[0],
+      result.end[1],
+      10,
+      10
+    ),
+    path = A.getPath();
+  for (let i = 0, len = path.length; i < len; i++) {
+    setTimeout(() => {
+      posArray[path[i][0]][path[i][1]].classList.add('active');
+    }, i * 200);
+    setTimeout(() => {
+      posArray[path[i][0]][path[i][1]].classList.remove('active');
+    }, i * 200 + 200);
+  }
 });
 //
-button.addEventListener('click', () => {
-    let result = getMatrix(posArray),
-        arr = result.arr,
-        D = new DfsFind(arr, result.start[0], result.start[1], 10, 10),
-        path = D.getPath();
-    for (let i = 0, len = path.length; i < len; i++) {
-        setTimeout(() => {
-            posArray[path[i][0]][path[i][1]].classList.add('active');
-        }, i * 200);
-        setTimeout(() => {
-            posArray[path[i][0]][path[i][1]].classList.remove('active');
-        }, i * 200 + 200);
-    }
+dfs.addEventListener('click', () => {
+  let result = getMatrix(posArray),
+    arr = result.arr;
+  (D = new DfsFind(arr, result.start[0], result.start[1], 10, 10)),
+    (path = D.getPath());
+  for (let i = 0, len = path.length; i < len; i++) {
+    setTimeout(() => {
+      posArray[path[i][0]][path[i][1]].classList.add('active');
+    }, i * 200);
+    setTimeout(() => {
+      posArray[path[i][0]][path[i][1]].classList.remove('active');
+    }, i * 200 + 200);
+  }
+});
+
+bfs.addEventListener('click', () => {
+  let result = getMatrix(posArray);
+  let arr = result.arr;
+  let bfs = new BFS(arr, result.start, result.end, 10, 10);
+  path = bfs.findPath()
+  for (let i = 0, len = path.length; i < len; i++) {
+      setTimeout(() => {
+          posArray[path[i][0]][path[i][1]].classList.add('active');
+      }, i * 200);
+      setTimeout(() => {
+          posArray[path[i][0]][path[i][1]].classList.remove('active');
+      }, i * 200 + 200);
+  }
 });
 //
-controler.addEventListener('click', (e) => {
-    let target = e.target;
-    if (activeHelper && activeHelper === target) {
-        target.classList.remove('active');
-        activeHelper = null;
-    } else if (target.classList.contains('helper') && activeHelper !== target) {
-        if (activeHelper) {
-            activeHelper.classList.remove('active');
-            target.classList.add('active');
-        } else {
-            target.classList.add('active');
-        }
-        activeHelper = target;
-        
-    }
-});
-container.addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    quickBuild = true;
-    let target = e.target;
+controler.addEventListener('click', e => {
+  let target = e.target;
+  if (activeHelper && activeHelper === target) {
+    target.classList.remove('active');
+    activeHelper = null;
+  } else if (target.classList.contains('helper') && activeHelper !== target) {
     if (activeHelper) {
-        switch (activeHelper.innerText) {
-        case 'Build' :
-            if (target.className === 'item') {
-                target.classList.add('block');
-            }
-            break;
-        case 'Destory':
-            target.classList.remove('block');
-            break;
-        case 'Start':
-            if (!startElement && target.className === 'item') {
-                target.classList.add('start');
-                startElement = target;
-            } else if (target.className === 'item') {
-                startElement.className = 'item';
-                target.className = 'item start';
-                startElement = target;
-            }
-            break;
-        case 'End':
-            if (!endElement && target.className === 'item') {
-                target.classList.add('end');
-                endElement = target;
-            } else if (target.className === 'item') {
-                endElement.className = 'item';
-                target.className = 'item end';
-                endElement = target;
-            }
-            break;
-        default:
-            break;
-        }
+      activeHelper.classList.remove('active');
+      target.classList.add('active');
+    } else {
+      target.classList.add('active');
     }
-
+    activeHelper = target;
+  }
+});
+container.addEventListener('mousedown', e => {
+  e.preventDefault();
+  quickBuild = true;
+  let target = e.target;
+  if (activeHelper) {
+    switch (activeHelper.innerText) {
+      case 'Build':
+        if (target.className === 'item') {
+          target.classList.add('block');
+        }
+        break;
+      case 'Destory':
+        target.classList.remove('block');
+        break;
+      case 'Start':
+        if (!startElement && target.className === 'item') {
+          target.classList.add('start');
+          startElement = target;
+        } else if (target.className === 'item') {
+          startElement.className = 'item';
+          target.className = 'item start';
+          startElement = target;
+        }
+        break;
+      case 'End':
+        if (!endElement && target.className === 'item') {
+          target.classList.add('end');
+          endElement = target;
+        } else if (target.className === 'item') {
+          endElement.className = 'item';
+          target.className = 'item end';
+          endElement = target;
+        }
+        break;
+      default:
+        break;
+    }
+  }
 });
 
-container.addEventListener('mousemove', (e) => {
-    if (quickBuild && activeHelper && activeHelper.innerText === 'Build' && e.target.className === 'item' ) {
-        e.target.classList.add('block');
-    }
+container.addEventListener('mousemove', e => {
+  if (
+    quickBuild &&
+    activeHelper &&
+    activeHelper.innerText === 'Build' &&
+    e.target.className === 'item'
+  ) {
+    e.target.classList.add('block');
+  }
 });
 document.addEventListener('mouseup', () => {
-    quickBuild = false;
+  quickBuild = false;
 });
 //Function
 
 function init() {
-    let rows = container.getElementsByClassName('row');
-    for (let i = 0, len = rows.length; i < len; i++) {
-        let items = rows[i].getElementsByClassName('item');
-        posArray[i] = [];
-        for (let j = 0, l = items.length; j < l; j++) {
-            posArray[i].push(items[j]);
-        }
+  let rows = container.getElementsByClassName('row');
+  for (let i = 0, len = rows.length; i < len; i++) {
+    let items = rows[i].getElementsByClassName('item');
+    posArray[i] = [];
+    for (let j = 0, l = items.length; j < l; j++) {
+      posArray[i].push(items[j]);
     }
+  }
 }
 
 function getMatrix(eleArray) {
-    let l = eleArray[0].length,
-        arr = [],
-        start = [],
-        end = [],
-        ele = null;
-    for (let i = 0, len = eleArray.length; i < len; i++) {
-        arr[i] = [];
-        for (let j = 0; j < l; j++) {
-            ele = eleArray[i][j];
-            switch (ele.className) {
-            case 'item':
-                arr[i][j] = 0;
-                break;
-            case 'item block':
-                arr[i][j] = 1;
-                break;
-            case 'item start':
-                start.push(i, j);
-                arr[i][j] = 2;
-                break;
-            case 'item end':
-                end.push(i, j);            
-                arr[i][j] = 3;
-                break;
-            default:
-                break;
-            }
-        }
-        
+  let l = eleArray[0].length,
+    arr = [],
+    start = [],
+    end = [],
+    ele = null;
+  for (let i = 0, len = eleArray.length; i < len; i++) {
+    arr[i] = [];
+    for (let j = 0; j < l; j++) {
+      ele = eleArray[i][j];
+      switch (ele.className) {
+        case 'item':
+          arr[i][j] = 0;
+          break;
+        case 'item block':
+          arr[i][j] = 1;
+          break;
+        case 'item start':
+          start.push(i, j);
+          arr[i][j] = 2;
+          break;
+        case 'item end':
+          end.push(i, j);
+          arr[i][j] = 3;
+          break;
+        default:
+          break;
+      }
     }
-    return {
-        start : start,
-        end : end,
-        arr : arr
-    };
-
+  }
+  return {
+    start: start,
+    end: end,
+    arr: arr,
+  };
 }
-
-
